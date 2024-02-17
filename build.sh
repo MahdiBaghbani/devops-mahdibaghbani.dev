cfc93e17381b54aeca5288568ae56dd7d70a0c32
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2024-02-17 13:03:21 +0330

    add: build script

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# halt on errors.
+set -e
+
+function updateGitRepository () {
+  cd  "${1}"
+  git fetch --all
+  git reset --hard origin/master
+  git pull
+  cd "${2}"
+}
+
+# find this scripts location.
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "${SOURCE}" ]; do # resolve ${SOURCE} until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "${SOURCE}")
+   # if ${SOURCE} was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ ${SOURCE} != /* ]] && SOURCE=${DIR}/${SOURCE}
+done
+DIR=$( cd -P "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )
+
+# go to the scripts directory.
+cd "${DIR}" || exit
+
+git submodule update --init --recursive
+
+updateGitRepository ./mahdibaghbani.dev-volumes/mahdibaghbani.dev "${DIR}"
+updateGitRepository ./mahdibaghbani.dev-volumes/mahdibaghbani.dev/themes/erfan "${DIR}"
+
+cd ./mahdibaghbani.dev-volumes/mahdibaghbani.dev
+zola build
